f0b5c8bf17cd445ce3a6d3284d814b21461cf00b
roman90x <roman90x@icloud.com>
2023-03-17 00:01:43 +0600

    Initial commit

---
+++ src/main/java/me/roman/onlinestoreofsocks/OnlineStoreOfSocksApplication.java
@@ -0,0 +1,13 @@
+package me.roman.onlinestoreofsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreOfSocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreOfSocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/roman/onlinestoreofsocks/OnlineStoreOfSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package me.roman.onlinestoreofsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreOfSocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
